2ffb3392aad77d61afbe988a1dd86a3ce7ab8bab
Nicholas Levenson <nicklevenson@gmail.com>
2020-11-30 16:28:04 -0800

    commit tree 6ce07bf7be322078661dd11d2347dd1930634b4b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,45 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    
   end
+
+  get '/articles' do
+    @articles = Article.all 
+    erb :index
+  end
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    article = Article.create(params)
+    redirect "/articles/#{article.id}"
+  end
+
+  get '/articles/:id' do 
+    @article = Article.find_by(id: params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @id = params[:id]
+    @article = Article.find_by(id: @id)
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    article = Article.find_by(id: params[:id])
+    article.update(title: params[:title])
+    article.update(content: params[:content])
+    redirect "/articles/#{article.id}"
+  end
+
+  delete '/articles/:id' do
+    article = Article.find_by(id: params[:id])
+    article.destroy
+  end
+
+  
+ 
 end
